7e52d8258b7145a2a8dc3966aa754df7e67a0210
seunglee <seunglee10@users.noreply.github.com>
2026-06-10 22:34:52 +0900

    feat: env setting config 구현

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "Week16 Mini Board"
-    debug: bool = True
+    API_FOOTBALL_KEY: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
