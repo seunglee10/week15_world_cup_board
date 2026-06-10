95422352a0c50a49f5c30ceb88e1fa512a84f0e0
seunglee <seunglee10@users.noreply.github.com>
2026-06-10 20:02:39 +0900

    feat: 국가별 선수 조회 기능 구현

--- test_service.py
+++ test_service.py
@@ -1,8 +1,12 @@
 from app.football.service import search_players
-
+from app.football.service import get_team_players
 
 print(search_players("son"))
 print(search_players("SON"))
 print(search_players("heung"))
 print(search_players("sonheungmin"))
 print(search_players("ronaldo"))
+print(get_team_players("korea"))
+print(get_team_players("KOREA REPUBLIC"))
+print(get_team_players("korearepublic"))
+print(get_team_players("brazil"))
